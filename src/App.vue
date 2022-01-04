22386a369b9422ebdc2234f41bd7e89695c7c721
Philippe CANCELLIER <philippe.cancellier@pm.me>
2022-01-04 21:07:14 +0100

    little customization

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app clipped v-model="drawer">
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+          <v-list-item to="/about">
+            <v-list-item-icon>
+              <v-icon>mdi-information</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
+    <v-app-bar app dense clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify Capacitor</v-toolbar-title>
       <v-spacer></v-spacer>
-
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
@@ -38,18 +35,20 @@
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer app> &copy; Fifcan.ORG </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
+  name: "App",
   data: () => ({
-    //
+    drawer: true,
   }),
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,13 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import fr from 'vuetify/lib/locale/fr';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import fr from "vuetify/lib/locale/fr";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    lang: {
-      locales: { fr },
-      current: 'fr',
-    },
+  theme: { dark: true },
+  lang: {
+    locales: { fr },
+    current: "fr",
+  },
 });
